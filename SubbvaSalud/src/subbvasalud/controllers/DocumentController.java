8e151bdae4f31da26003970643802ce1068417cc
dquinteros <daniel.quinterosc@usach.cl>
2013-12-25 22:22:39 -0400

    fuuu

---
+++ SubbvaSalud/src/subbvasalud/controllers/DocumentController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package subbvasalud.controllers;
+
+import subbvasalud.models.SolicitudDeReembolso;
+
+/**
+ *
+ * @author damage
+ */
+public class DocumentController {
+    
+    SolicitudDeReembolso s;
+    
+    
+    
+}
